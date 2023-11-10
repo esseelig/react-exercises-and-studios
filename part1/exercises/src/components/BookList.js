458112838eda38ac54b8350ad8d0e65488609f81
esseelig <118030730+esseelig@users.noreply.github.com>
2023-11-10 17:54:09 -0600

    Completed React Part 1 exercises, part 1

--- part1/exercises/src/components/BookList.js
+++ part1/exercises/src/components/BookList.js
@@ -6,10 +6,10 @@ export default function BookList() {
 
    return (
       <div>
-         <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <h3>Books I Read This Month</h3>
+         <img src="https://m.media-amazon.com/images/I/51dWimSLnKL._SS135_.jpg" alt="Confessions of a Prep School Mommy Handler: A Memoir" />
+         <img src="https://m.media-amazon.com/images/I/51-9RjyQIbL._SS135_.jpg" alt="The Come Up: An Oral History of the Rise of Hip-Hop" />
+         <img src="https://m.media-amazon.com/images/I/51e8yZT4xxL._SS135_.jpg" alt="All These Things That I've Done: My Insane, Improbable Rock Life" />
       </div>      
    );
 }
