34f8838df89725e23cc518b749c31053e0b9bd9f
Eric Seelig <esseelig@gmail.com>
2023-11-17 15:17:26 -0600

    Completed React Part 3 exercises

--- part3/exercises/src/components/Introduction.jsx
+++ part3/exercises/src/components/Introduction.jsx
@@ -1,11 +1,11 @@
 export default function HobbyIntroduction () {
    return(
       <div>
-         <h2>3 Fun Facts About Needlepoint</h2>
+         <h2>3 Fun Facts About Photography</h2>
          <ol>
-            <li>Needlepoint uses a specialized canvas as it's base.</li>
-            <li>The most basic stitch is called a tent stitch.</li>
-            <li>Needlepoint uses a lot of different kinds of fibers.</li>
+            <li>In the days of film, with the same photography hardware and nothing else different, you would capture a darker scene by using a more sensitive, higher ISO roll of film. In today's digital SLRs and phone cameras, higher ISOs for darker scenes are instead achieved by the hardware making its camera sensor more sensitive to light during capture.</li>
+            <li>Many of the tinted camera filters that became popular in the early days of Instagram and VSCO for their sense of mood and tone were based on the look of photos shot with various types of expired film.</li>
+            <li>In an indoor environment, it's generally preferred if possible when shooting with a camera flash to bounce the light of the flash off of the ceiling rather than shooting your subject with the flash directly, as this results in more flattering, more diffused lighting.</li>
          </ol>
       </div>
    );
